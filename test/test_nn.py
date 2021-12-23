17b9ed2c741e04fc3a3842457d1f1024eef144f0
soulitzer <soulitzer@gmail.com>
2021-12-23 13:11:47 -0500

    Add forward AD formula for convolution and some others
    ghstack-source-id: aff3ddd18f0404f9edeba71644c7f19887e19eea
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69956

--- test/test_nn.py
+++ test/test_nn.py
@@ -13408,6 +13408,8 @@ class TestNNDeviceType(NNTestCase):
         self.assertEqual(gy_expect, y.grad)
 
     def test_conv3d_same_padding_backward(self, device):
+        check_forward_ad = torch.device(device).type != 'xla'
+
         # Test F.conv3d gradients work with padding='same'
         x = torch.rand(1, 1, 1, 11, 12, device=device, requires_grad=True)
         y = torch.rand(1, 1, 1, 2, 5, device=device, requires_grad=True)
@@ -13424,6 +13426,12 @@ class TestNNDeviceType(NNTestCase):
         self.assertEqual(gy_expect, y.grad)
         x.grad, y.grad = None, None
 
+        gradcheck(lambda x, y: F.conv3d(x, y, padding='same', dilation=2), (x, y),
+                  check_forward_ad=check_forward_ad, nondet_tol=1e-5)
+        if torch.device(device).type == 'cpu':
+            gradgradcheck(lambda x, y: F.conv3d(x, y, padding='same', dilation=2), (x, y),
+                          check_fwd_over_rev=check_forward_ad)
+
         # Asymmetric padding
         y = torch.rand(1, 1, 1, 4, 4, device=device, requires_grad=True)
         z = F.conv3d(x, y, padding=2)[..., 1:, 1:]
@@ -13435,6 +13443,12 @@ class TestNNDeviceType(NNTestCase):
         z.sum().backward()
         self.assertEqual(gx_expect, x.grad)
         self.assertEqual(gy_expect, y.grad)
+
+        gradcheck(lambda x, y: F.conv3d(x, y, padding='same'), (x, y),
+                  check_forward_ad=check_forward_ad, nondet_tol=1e-5)
+        if torch.device(device).type == 'cpu':
+            gradgradcheck(lambda x, y: F.conv3d(x, y, padding='same'), (x, y),
+                          check_fwd_over_rev=check_forward_ad)
 
     def test_conv1d_valid_padding_backward(self, device):
         # Test F.conv1d gradients work with padding='valid'
@@ -13463,6 +13477,8 @@ class TestNNDeviceType(NNTestCase):
         self.assertEqual(gy_expect, gy_actual)
 
     def test_conv3d_valid_padding_backward(self, device):
+        check_forward_ad = torch.device(device).type != 'xla'
+
         # Test F.conv3d gradients work with padding='valid'
         x = torch.rand(1, 1, 1, 1, 10, device=device, requires_grad=True)
         y = torch.rand(1, 1, 1, 1, 4, device=device, requires_grad=True)
@@ -13474,6 +13490,9 @@ class TestNNDeviceType(NNTestCase):
         gx_actual, gy_actual = x.grad, y.grad
         self.assertEqual(gx_expect, gx_actual)
         self.assertEqual(gy_expect, gy_actual)
+
+        gradcheck(lambda x, y: F.conv3d(x, y, padding='valid'), (x, y), check_forward_ad=check_forward_ad)
+        gradgradcheck(lambda x, y: F.conv3d(x, y, padding='valid'), (x, y), check_fwd_over_rev=check_forward_ad)
 
     @skipMeta
     @parametrize_test("input_shape,transposed,dilated,groups,layout,backend_expected", [
@@ -18670,8 +18689,8 @@ class TestNNDeviceType(NNTestCase):
         for sd in seeds:
             torch.manual_seed(sd)
             x = torch.randn(1, 12, 12, device=device, requires_grad=True)
-            gradcheck(func, [x])
-            gradgradcheck(func, [x])
+            gradcheck(func, [x], check_forward_ad=True)
+            gradgradcheck(func, [x], check_fwd_over_rev=True)
             if device == 'cpu':
                 test_dtype(func, x, torch.bfloat16)
 
